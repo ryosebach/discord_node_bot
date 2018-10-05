4a68f7759a43d53c3e5c99fdb5cc5b12c723a26d
ryosebach <ryo.freezia@gmail.com>
2018-10-05 09:18:18 +0900

    chore: logging on login

--- src/server/app/helpers/bot.ts
+++ src/server/app/helpers/bot.ts
@@ -4,11 +4,15 @@
  * (c) 2018 ryosebach
  */
 import {Channel, Message, TextChannel} from 'discord.js';
+import * as log4js from 'log4js';
 
 import * as bot_controller from 'server/app/controllers/bot';
 import DiscordBot from 'server/app/middleware/discordBot';
 
+const logger = log4js.getLogger('console');
+
 export const ready = async (): Promise<void> => {
+    logger.info('log in discord');
     DiscordBot.paymentChannel = await DiscordBot.findChannel('payment');
     DiscordBot.newsChannel = await DiscordBot.findChannel('news');
 };
