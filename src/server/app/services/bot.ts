552c8123b4cff896d805c1e9754a25d5660fcd30
ryosebach <ryo.freezia@gmail.com>
2018-10-05 21:33:38 +0900

    feat: limit bot-message-deleting-method to only specified words

--- src/server/app/services/bot.ts
+++ src/server/app/services/bot.ts
@@ -5,6 +5,7 @@
  */
 import {Attachment, Message, TextChannel} from 'discord.js';
 import * as log4js from 'log4js';
+import * as moment from 'moment';
 
 import Puppeteer from 'server/app/middleware/puppeteer';
 import * as cheerio from 'server/app/services/cheerio';
@@ -12,18 +13,21 @@ import * as yahooWeather from 'server/app/utils/yahooServices';
 
 const logger = log4js.getLogger('console');
 
-const clearBotMessage = async (channel: TextChannel): Promise<void> => {
+const clearBotMessage = async (channel: TextChannel, name?: string): Promise<void> => {
     const messages = await channel.fetchMessages({ limit: 10});
     for (const val of messages.array()) {
-      if (val.author.username !== 'Nyanko') { continue; }
-      val.delete();
+        if (val.author.username !== 'Nyanko' ||
+            (name && !val.attachments.first().filename.match(new RegExp(`${name}`)))) {
+            continue;
+        }
+        val.delete();
     }
 };
 
 export const sendWeatherInfo = async (mes: Message): Promise<void> => {
-    await clearBotMessage(mes.channel as TextChannel);
+    await clearBotMessage(mes.channel as TextChannel, 'weather');
     const buffer = await Puppeteer.screenshotDOMElement('https://weather.yahoo.co.jp/weather/jp/13/4410.html', 'div .forecastCity', 2);
-    const attachment = new Attachment(buffer, 'weather.png');
+    const attachment = new Attachment(buffer, `weather-${moment().format('HHmmss')}.png`);
     await mes.delete();
     await mes.channel.send('', attachment);
 };
@@ -31,18 +35,18 @@ export const sendWeatherInfo = async (mes: Message): Promise<void> => {
 export const sendRainCloudGif = async (mes: Message): Promise<void> => {
     const rainCloudInfo = mes.content.replace(/( |　)+/g, ' ');
     const rainCloudInfos = rainCloudInfo.split(' ');
-    await clearBotMessage(mes.channel as TextChannel);
+    await clearBotMessage(mes.channel as TextChannel, 'rain_cloud');
     const url = yahooWeather.rainCloudUrl[rainCloudInfos[1] ? rainCloudInfos[1] : 'kanto'];
     const buffer = await cheerio.fetchRainCloudRadorGif(url);
-    const attachment = new Attachment(buffer, 'weather.gif');
+    const attachment = new Attachment(buffer, `rain_cloud-${moment().format('HHmmss')}.gif`);
     await mes.delete();
     await mes.channel.send('', attachment);
 };
 
 export const sendKantoTrainInfo = async (mes: Message): Promise<void> => {
-    await clearBotMessage(mes.channel as TextChannel);
+    await clearBotMessage(mes.channel as TextChannel, 'train_info');
     const buffer = await Puppeteer.screenshotDOMElement('https://transit.yahoo.co.jp/traininfo/area/4/', 'div #mdStatusTroubleLine', 5);
-    const attachment = new Attachment(buffer, 'weather.png');
+    const attachment = new Attachment(buffer, `train_info-${moment().format('HHmmss')}.png`);
     await mes.delete();
     await mes.channel.send('', attachment);
 };
